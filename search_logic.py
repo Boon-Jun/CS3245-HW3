170687e8454ce8bc3060519493027cb9e42e2ab9
Boon Jun <boon-jun1996@hotmail.com>
2019-03-20 12:42:11 +0800

    Minor comments changes in search_logic.py

--- search_logic.py
+++ search_logic.py
@@ -11,7 +11,6 @@ def calculateLength(coordinates):
     return math.sqrt(i)
 
 def executeSearch(queryString, term_dict, postings, lengths_file):
-    #Load number of docs
 
     queryTermSet = {}
     queryWeights = {}
@@ -25,23 +24,25 @@ def executeSearch(queryString, term_dict, postings, lengths_file):
         else:
             queryTermSet[term][0] += 1
 
+    #Calculates weights for query without normalization
     for term in queryTermSet:
         infoPair = queryTermSet[term]
         if infoPair[1] != 0:
             queryWeights[term] = (1 + math.log10(infoPair[0])) * (math.log10(totalNumberOfDocs/infoPair[1]))
 
-    #Calculating term weights * docId Weights
+    #Calculates cosine scoring minus the normalization
     for term in queryWeights:
         postingList = loadPostingList(term, term_dict, postings)
         for posting in postingList:
-            docId = posting[0]#Depends on the actual position once implemented in the future
+            docId = posting[0]
             if docId not in scores:
                 scores[docId] = 0
-            scores[docId] += queryWeights[term] * (1 + math.log10(posting[1]))#Depends on the actual position once implemented(TermFreq in Doc)
+            scores[docId] += queryWeights[term] * (1 + math.log10(posting[1]))
 
-    #Normalized scores
+    #Normalization for docIds
     for docId in scores:
         scores[docId] = scores[docId]/getLengthOfDoc(docId, lengths_file)
 
+    #Retrieve Top 10 Documents with a heap
     topList = heapq.nlargest(10, ([scores[docId], docId] for docId in scores), key = itemgetter(0))
     return [pair[1] for pair in sorted(topList, key = lambda pair: (-pair[0], pair[1]))]
